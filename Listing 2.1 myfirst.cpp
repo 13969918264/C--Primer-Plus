9cb13dd987bf8a87c423d9a88addf68127e94f61
赵子豪 <13969918264@163.com>
2019-07-01 20:27:28 +0800

    Create Listing 2.1 myfirst.cpp

---
+++ Listing 2.1 myfirst.cpp
@@ -0,0 +1,17 @@
+/*
+	姓名：赵子豪
+	日期：2019年7月1日
+*/
+
+//  myfirst.cpp -- displays a message
+
+#include <iostream>                             //  a PREPROCESSOR directive
+int main()                                      //  function header
+{                                               //  start of function body
+	using namesapce std;                        //  make definition visianle
+	cout << "Come up and C++ me some time.";    //  message
+	cout << end1;                               //  start a new line
+	cout << "You won't regret it!" << end1;     //  more output
+
+	return 0;                                   //  terminate main()
+}                                               //  end of function body
