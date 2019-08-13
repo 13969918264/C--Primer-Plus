dec57aa4ffdb8664466096bb317b48fe6d0025fc
赵子豪 <13969918264@163.com>
2019-08-13 21:24:15 +0800

    Create Listing 3.1 limits.cpp

---
+++ Listing 3.1 limits.cpp
@@ -0,0 +1,35 @@
+/*
+    姓名：赵子豪
+    日期：2019年8月13日
+*/
+//  limits.cpp -- come integer limits
+
+#include <iostream>
+#include <climits>          //  use limits.h for order systems
+
+int main()
+{
+    using namespace std;
+    int n_int = TNT_MAX;            //  initialize n_int to max int value
+    short n_short = SHRT_MAX;           //  symbols defined in climits file
+    long n_long = LONG_MAX;
+    long long n_llong = LLONG_MAX;
+
+    //  sizeof operator yields size of type or of varibles
+    cout << "int is " << sizeof (int) << " bytes." << endl;
+    cout << "int is " << sizeof n_short << " bytes." << endl;
+    cout << "int is " << sizeof n_long << " bytes." << endl;
+    cout << "long long is " << sizeof n_llong << " bytes." << endl;
+    cout << engl;
+
+    cout << "Maximum values: " << endl;
+    cout << "int : " << n_int << endl;
+    cout << "short : " << n_short << endl;
+    cout << "long : " << n_long << endl;
+    cout << "long long : " << n_llong << endl;
+    
+    cout << "Minimum int value = " << INT_MIN << endl;
+    cout << "Bits per type = " << CHAR_BIT << endl;
+    
+    return 0;
+}
